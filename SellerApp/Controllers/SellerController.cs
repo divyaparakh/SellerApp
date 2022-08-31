d1385a101d8554f0a07e9b58975bbe6ce7219936
divyaparakh <parakhdivya07@gmail.com>
2022-09-01 01:38:30 +0530

    Add project files.

---
+++ SellerApp/Controllers/SellerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SellerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
